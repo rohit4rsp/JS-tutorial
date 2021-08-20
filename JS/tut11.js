9c975cd705ee2230e75e59b1ffc2526227512de7
rohit4rsp <80252100+rohit4rsp@users.noreply.github.com>
2021-08-20 19:59:16 +0530

    updates
    javascript functions,loops,window

---
+++ JS/tut11.js
@@ -0,0 +1,16 @@
+console.log('This is tut 11');
+// let b= window;
+// console.log(b);          
+
+let a = window.document;
+// alert('hello harry');
+// a = prompt('This will destroy your computer. Type your name');
+
+// a = confirm('Are you sure you want to delete this page?');
+a = window.innerHeight;
+a = innerWidth;
+a = scrollX;
+a = scrollY;
+a = location.toString();  //mathods in location!!
+a = window.history;
+console.log(a);          
