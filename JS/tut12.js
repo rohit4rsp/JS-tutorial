2a181eb2c4012702d299d998297f4705e87020fd
rohit4rsp <80252100+rohit4rsp@users.noreply.github.com>
2021-08-21 20:11:40 +0530

    update

---
+++ JS/tut12.js
@@ -0,0 +1,29 @@
+// let a= prompt('any problem?');
+// console.log(a);
+
+// confirm('do u agree?');
+
+console.log('Welcome to tut 12.js');
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0];
+
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+
+// a = document.links[0];
+
+// use document.images and document.scripts and print the list of images and scripts on an html page
+
+console.log(a);
+
+
+// Exercise 1
+// You have to create a variable which is a string containing the text which is a link you are interested in. 
+
+// You have to fetch all the links from a given page which contains this text
+
+// codewithharry.com 
+// javascript
